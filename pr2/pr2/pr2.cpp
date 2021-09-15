f4ac4da451b157c209c3909faefe810664d4206b
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2021-09-15 15:07:24 +0300

    add lab 2.1

---
+++ pr2/pr2/pr2.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_02.cpp
+// <Добош Юлія Олександрівна >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 7
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	double Pi = 4 * atan(1.), a, z1, z2;
+
+	cout << "a = ";
+	cin >> a;
+
+	z1 = (cos(3.0 / 8 * Pi - a / 4) * cos(3.0 / 8 * Pi - a / 4)) - (cos(11.0 / 8 * Pi + a / 4) * cos(11.0 / 8 * Pi + a / 4));
+		
+	z2 = (sqrt(2) / 2) * sin(a / 2);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
+
+
+	
+
+
+
